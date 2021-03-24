278a0e20eb60eaca3e5a99a3b835bdac5a89b135
mitdonda008 <79262032+mitdonda008@users.noreply.github.com>
2021-03-24 13:36:33 +0100

    less than 100

--- Question1.java
+++ Question1.java
@@ -15,7 +15,7 @@ public class Question1 {
     public static void main(String[] args) {
 		int sum = 0;
 		
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
 		if (i % 3 == 0 || i % 7 == 0)
 		
 			sum += i;
